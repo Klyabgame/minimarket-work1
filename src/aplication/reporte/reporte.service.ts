5b2422b225d7b72dbeee8f3185aa20b38021863d
franz <gavino_10@hotmail.es>
2024-08-13 17:26:20 -0500

    fix:add reports

--- src/aplication/reporte/reporte.service.ts
+++ src/aplication/reporte/reporte.service.ts
@@ -1,6 +1,30 @@
+/* import PdfPrinter from 'pdfmake';
+import fs from 'fs';
+const fonts = {
+    Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+    }
+};
 
 export class ReporteService {
 
     constructor(){}
-    
-}
+
+  const printer = new PdfPrinter(fonts);
+  
+  const docDefinition = {
+    // ...
+  };
+  const options = {
+    // ...
+  }
+  
+  const pdfDoc = printer.createPdfKitDocument(docDefinition, options);
+  pdfDoc.pipe(fs.createWriteStream('document.pdf'));
+  pdfDoc.end();
+  
+
+} */
